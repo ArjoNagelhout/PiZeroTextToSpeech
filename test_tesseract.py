2570bc560a486bd2ae1957af11dace5db4e02d33
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-10-19 01:55:23 +0200

    Update test_tesseract.py

--- test_tesseract.py
+++ test_tesseract.py
@@ -72,6 +72,7 @@ def speak(input_string):
     #input_string = re.sub(r'\W+', '', input_string)
     input_string = input_string.replace('\'', '')
     input_string = input_string.replace('"', '')
+    input_string = input_string.replace('\n', '_')
     input_string = input_string.replace(' ', '_')
     subprocess.run(['espeak -s 120 --stdout > /home/pi/speak.wav '+input_string], shell=True)
     subprocess.run(["aplay /home/pi/speak.wav"], shell=True)
