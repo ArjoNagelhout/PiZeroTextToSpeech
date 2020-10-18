9f1ab1b9276201f594ca54b295d6b547806d72d6
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-10-18 22:54:26 +0200

    Update test_espeak.py

--- test_espeak.py
+++ test_espeak.py
@@ -1,13 +1,13 @@
 import subprocess
 
-string_to_speak = "Welcome to the new things here, it is awesome isn't it?"
+string_to_speak = 'Welcome to the new things here, it is awesome isn't it?'
 
 
 
 
 
 def speak(input_string):
-    subprocess.run(["espeak", string_to_speak, "--stdout > /home/pi/speak.wav"], shell=True)
+    subprocess.run(["espeak "+string_to_speak+" --stdout > /home/pi/speak.wav"], shell=True)
     subprocess.run(["aplay /home/pi/speak.wav"], shell=True)
 
 speak(string_to_speak)
