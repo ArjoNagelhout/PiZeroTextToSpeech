8d0292730b0645ed389f6eb5d8403bbaa0768821
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-10-18 22:49:50 +0200

    Update test_espeak.py

--- test_espeak.py
+++ test_espeak.py
@@ -1,4 +1,23 @@
 import subprocess
-string_to_speak = "Hello my friend. I am currently talking to you."
 
-speaking_subprocess = subprocess.run(["espeak", string_to_speak, "--stdout", "|", "aplay"])
+string_to_speak = "Welcome to the new things here, it is awesome isn't it?"
+
+#cmd_beg= 'espeak '
+#cmd_end= 'aplay /home/pi/Text.wav' # To play back the stored .wav file and to d$
+#cmd_out= '--stdout > /home/pi/Text.wav ' # To store the voice file
+
+#text = input("Enter the Text: ")
+#print(text)
+
+#Replacing ' ' with '_' to identify words in the text entered
+#text = text.replace(' ', '_')
+
+#Calls the Espeak TTS Engine to read aloud a Text
+
+
+
+def speak(input_string):
+    subprocess.run(["espeak --stdout > /home/pi/speak.wav "+input_string], shell=True)
+    subprocess.run(["aplay /home/pi/speak.wav"], shell=True)
+
+speak(string_to_speak)
