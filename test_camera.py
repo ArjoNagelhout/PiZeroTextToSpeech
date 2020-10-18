377ea75219b5f3d210862a923e048b7019d59f2f
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-10-18 22:08:30 +0200

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -7,5 +7,5 @@ height = 600
 datetime_string = datetime.today().strftime('%Y-%m-%d-%H%M%S')
 print(datetime_string)
 output_string = "/home/pi/pictures/" + datetime_string + ".jpg"
-take_picture = subprocess.run(["raspistill", "-o", output_string, "-w", str(width), "-h", str(height), "-t", "0"])
+take_picture = subprocess.run(["raspistill", "-o", output_string, "-w", str(width), "-h", str(height), "-t", "0"], shell=True)
 print("taken picture" + output_string)
