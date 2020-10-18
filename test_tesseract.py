9ae7eca0a27fe28bb8113b81ca1cb46924ec55b1
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-10-19 00:51:46 +0200

    Update test_tesseract.py

--- test_tesseract.py
+++ test_tesseract.py
@@ -1,50 +1,70 @@
 import subprocess
 from datetime import datetime
+from picamera import PiCamera
 import RPi.GPIO as GPIO
-#import time
 
+# Picture dimensions
 width = 600
 height = 600
 
-# SET GPIO SETTINGS
+# Pictures directory
+directory_string = "/home/pi/pictures/"
+
+# Initialize camera
+camera = PiCamera()
+
+# Set pin modes for GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(24,GPIO.OUT)
 
 
-datetime_string = datetime.today().strftime('%Y-%m-%d-%H%M%S')
-print(datetime_string)
-output_string = "/home/pi/pictures/" + datetime_string + ".jpg"
+# Function that takes a picture, post-processes it, uses tesseract to convert it to text and converts that to audio using espeak. 
+def convert_image_to_audio():
+    # Step 1: Get datetime string
+    datetime_string = datetime.today().strftime('%Y-%m-%d-%H%M%S')
+    print("Step 1: Datetime string "+datetime_string)
 
-#Turn led on
-print("LED on")
-GPIO.output(24,GPIO.HIGH)
+    # Step 2: Turn the LED on
+    GPIO.output(24,GPIO.HIGH)
+    print("Step 2: LED on")
 
-# Take picture
-#take_picture = subprocess.run(["raspistill", "-o", output_string, "-w", str(width), "-h", str(height)], "-t", "0", shell=True)
-take_picture = subprocess.run(["raspistill -o "+output_string+" -w 600 -h 600 -t 0"], shell=True)
-print("taken picture " + output_string)
+    # Step 3: Take the picture
+    
+    # Old method:
+    #take_picture = subprocess.run(["raspistill", "-o", output_string, "-w", str(width), "-h", str(height)], "-t", "0", shell=True)
+    #take_picture = subprocess.run(["raspistill -o "+output_string+" -w 600 -h 600 -t 0"], shell=True)
 
-# Turn led off
-GPIO.output(24,GPIO.LOW)
+    # New method:
+    output_string = directory_string + datetime_string + ".jpg"
+    camera.capture(output_string)
+    print("Step 3: Taken picture " + output_string)
 
-# Convert picture to right format
+    # Step 4: Turn the LED off
+    GPIO.output(24,GPIO.LOW)
+    print("Step 4: LED off")
 
-# Run tesseract
+    # Step 5: Post-process the image using imagemagick
+    output_string_postprocessed = directory_string + datetime_string + "-postprocessed.jpg"
+    print("Step 5: Post-processed image "+output_string_postprocessed)
 
-# Speak converted text
-string_to_speak = 'Welcome to the new things here, it is awesome isn\'t it?'
+    # Step 6: Use Tesseract    
+    output_string_tesseract = directory_string + datetime_string + "-tesseract.txt"
+    print("Step 6: Performed tesseract conversion "+output_string_tesseract)
+
+    # Step 7: Get string from file
+
+    # Step 8: Speak string
+    speak("Speak string")
 
 
-# https://www.dexterindustries.com/howto/make-your-raspberry-pi-speak/
-# https://github.com/DexterInd/Raspberry_Pi_Speech/blob/master/speak_text.py
-
-
+# Function that takes an input string and automatically speaks it using espeak and aplay
 def speak(input_string):
+    # https://www.dexterindustries.com/howto/make-your-raspberry-pi-speak/
+    # https://github.com/DexterInd/Raspberry_Pi_Speech/blob/master/speak_text.py
+    
     input_string = input_string.replace('\'', '')
     input_string = input_string.replace('"', '')
     input_string = input_string.replace(' ', '_')
     subprocess.run(['espeak -s 120 --stdout > /home/pi/speak.wav '+input_string], shell=True)
     subprocess.run(["aplay /home/pi/speak.wav"], shell=True)
-
-speak(string_to_speak)
