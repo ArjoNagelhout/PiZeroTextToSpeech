cddcc8efb62459a76406e6b2c42033f6d9ebcbe0
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-10-18 21:29:16 +0200

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -3,5 +3,5 @@ import subprocess
 width = 600
 height = 600
 
-take_picture = subprocess.run(["raspistill", "-o", "~/pictures/image.jpg", "-w", width, "-h", height])
+take_picture = subprocess.run(["raspistill", "-o", "~/pictures/image.jpg", "-w", str(width), "-h", str(height)])
 print(take_picture.returncode)
